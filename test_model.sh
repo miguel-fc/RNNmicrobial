eca705cc368a35312b35e58c82f26b9ae2f34dcb
cnrrobertson <cnr.robertson@gmail.com>
2022-04-19 20:57:37 -0400

    Add train and test scripts

---
+++ test_model.sh
@@ -0,0 +1,49 @@
+export CUDA_VISIBLE_DEVICES=0
+
+# Neural net parameters
+export LAYERS="32"
+export LR="0.0003"
+
+# Save directories
+export LOGDIR=$PWD"/results/logs/"
+export DATADIR=$PWD"/datasets/"
+export CHECKDIR=$PWD"/results/checkpoints/"
+export RESDIR=$PWD"/results/validation_images/"
+export CHECKPOINT=500
+
+# Specific case
+export LOC="wells-1_microns-30_interp-20-32-32_hsv"
+
+echo Data from $DATADIR$LOC
+echo Logs at $LOGDIR$LOC.log
+echo Results at $DATRESDIR$LOC"_layers-"$LAYERS"_lr-"$LR
+echo Model checkpoints at $CHECKDIR$LOC"_layers-"$LAYERS"_lr-"$LR
+cd ..
+(python -u /home/connor/GDrive/SCGSR/connor_to_miguel/predrnn-pytorch/run.py \
+    --is_training 0 \
+    --device cuda \
+    --dataset_name mnist \
+    --train_data_paths $DATADIR$LOC-train.npz \
+    --valid_data_paths $DATADIR$LOC-test.npz \
+    --pretrained_model $CHECKDIR$LOC"_layers-"$LAYERS"_lr-"$LR"/model.ckpt-"$CHECKPOINT \
+    --save_dir $CHECKDIR$LOC"_layers-"$LAYERS"_lr-"$LR"-test" \
+    --gen_frm_dir $RESDIR$LOC"_layers-"$LAYERS"_lr-"$LR"-test" \
+    --model_name predrnn \
+    --reverse_input 1 \
+    --img_width 32 \
+    --img_channel 3 \
+    --input_length 10 \
+    --total_length 20 \
+    --num_hidden $LAYERS,$LAYERS,$LAYERS,$LAYERS \
+    --filter_size 5 \
+    --stride 1 \
+    --patch_size 4 \
+    --layer_norm 0 \
+    --scheduled_sampling 1 \
+    --sampling_stop_iter 50000 \
+    --sampling_start_value 1.0 \
+    --sampling_changing_rate 0.00002 \
+    --lr $LR \
+    --batch_size 1 \
+    --num_save_samples 5 \
+) 2>&1 | tee $LOGDIR$LOC"_layers-"$LAYERS"_lr-"$LR"-test".log
